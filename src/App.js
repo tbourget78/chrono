50849d8e5a754e0949557babe3d6e12487a194e1
Thomas Bourget <T.bourgetdev@outlook.fr>
2022-09-16 16:08:28 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Chrono from "./Chrono/Chrono";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Chrono />
+    </div>
+  );
+};
+
+export default App;
